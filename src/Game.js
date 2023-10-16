5e3e81ecb0042a5e63bd0554150058b10ad32186
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2023-10-16 12:48:52 +0200

    Det finns enemies nu

--- src/Game.js
+++ src/Game.js
@@ -2,6 +2,7 @@
 import Player from './Player.js'
 import InputHandler from './InputHandler.js'
 import UserInterface from './UserInterface.js'
+import Ghost from './assets/Ghost.js'
 
 export default class Game {
   constructor(width, height) {
@@ -15,6 +16,9 @@ export default class Game {
     this.player = new Player(this)
     this.InputHandler = new InputHandler(this)
     this.UserInterface = new UserInterface(this)
+    this.enemies = []
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
   }
 
 
@@ -24,10 +28,22 @@ export default class Game {
       this.gameTime += deltaTime
       this.player.update(deltaTime)
     }
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deltaTime
+    } 
+    this.enemies.forEach((enemy) => enemy.update(deltaTime))
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
+  }
+  addEnemy() {
+    this.enemies.push(new Ghost(this))
   }
   draw(context) {
     this.player.draw(context)
     this.UserInterface.draw(context)
+    this.enemies.forEach((enemy) => enemy.draw(context))
   }
 
 }
