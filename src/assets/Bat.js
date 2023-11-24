4e69203394f8b0ee0450a5ffe60b6746df23ae0f
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2023-11-24 12:56:16 +0100

    Enemysprite

--- src/assets/Bat.js
+++ src/assets/Bat.js
@@ -8,6 +8,52 @@ export default class Bat extends Enemy {
     this.x = this.game.width
     this.y = Math.random() * (this.game.height * 0.9 - this.height)
     this.speedX = Math.random() * -1.9 - 0.1
+    this.MaxSpeed = 7
+    this.fps = 20
+    this.timer = 0
+    this.interval = 1000 / this.fps
+    this.flip = false
 
+    const image = new Image()
+    image.src = spriteImage
+    this.image = image
+    this.frameX = 0
+    this.frameY = 1
+    this.maxFrame = 4
+    this.fps = 20
+    this.timer = 1
+    this.interval = 1000 / this.fps
+    this.flip = false
+  }
+  update(deltaTime) {
+    if (this.timer > this.interval) {
+      this.frameX++
+      this.timer = 0
+    } else {
+      this.timer += deltaTime
+    }
+    if (this.frameX >= this.maxFrame) {
+      this.frameX = 0
+    }
+  }
+  draw(context) {
+    if (this.flip) {
+      context.save()
+      context.scale(-1, 1)
+    }
+
+    context.drawImage(
+      this.image,
+      this.frameX * this.width,
+      this.frameY * this.height,
+      this.width,
+      this.height,
+      this.flip ? this.x * -1 - this.width : this.x,
+      this.y,
+      this.width,
+      this.height
+    )
+
+    context.restore()
   }
 }
