c55e2317a156c0520faafd6a3ce6a8fdf4b76bc2
Noel Johansson <noel.johansson@elev.ga.ntig.se>
2024-04-30 11:20:48 +0200

    server

--- src/Hiscore.js
+++ src/Hiscore.js
@@ -3,7 +3,7 @@ import Bottleneck from 'bottleneck'
 export default class Hiscore {
   constructor(game) {
     this.game = game
-    this.url = "http://localhost:3000"
+    this.url = "https://generated-hungry-pleasure.glitch.me"
     this.limiter = new Bottleneck({
       minTime: 200 // 5 requests per second
     })
@@ -17,20 +17,47 @@ export default class Hiscore {
     return this.limiter.schedule(() => fetch(this.url, options))
   }
 
+postScore() {
+    console.log("skicka score")
+    const data = { Hiscore }
+    fetch(`${this.url}/score`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((text) => {
+        console.log(text)
+      
+    })
+
+
+  }
+
   getScore() {
     console.log("hämta hiscore med jens kod")
 
   fetch(`${this.url}/score`)
-
-
+  .then((response) => response.text())
+  .then((text) => {
+    console.log(text)
+    const scores = JSON.parse(text)
+    const list = document.createElement("ul")
+    scores.forEach((score) => {
+      const item = document.createElement("li")
+      item.textContent = `${score.name}: ${score.score}`
+      list.appendChild(item)
+    })
+    element.appendChild(list)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
   }
-  postScore() {
-    console.log("skicka score")
 
-    const data = { Hiscore }
-
-
-  }
+  
 
   testApi () {
     
